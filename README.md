aa12c5c8185019af29e6c7f7be8d264825fe7cf5
cgetzen <charliegetzenlc@gmail.com>
2017-05-16 14:12:37 -0700

    Working demo #1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # divesb_upload
+
+Add password to .bash_profile
+
+export DIVE_PASS=yourpasswordhere
+source ~/.bash_profile
--- app.py
+++ app.py
@@ -1,15 +1,68 @@
 from flask import Flask, render_template, request
+import ftplib
+import json
+import os
+
 app = Flask(__name__)
 
+def connect_to_ftp():
+    server = "ftp.divesb.com"
+    user = "divesbcom"
+    password = os.environ["DIVE_PASS"]
+    return ftplib.FTP(server, user, password)
+
+def download_json():
+    ftp = connect_to_ftp()
+    ftp.cwd("/api/")
+    ftp.retrbinary("RETR menu.json", open("menu.json", 'wb').write)
+
+def edit_json(info, filename):
+    names = ["price", "description", "name"]
+    item = {name: info[name] for name in names}
+    item["image"] = filename
+
+    with open("menu.json") as f:
+        data = json.load(f)
+
+    for i, category in enumerate(data):
+        if category['name'] == info["Category"]:
+            data[i]["items"].append(item)
+
+    with open("menu.json.out", "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def upload_json():
+    ftp = connect_to_ftp()
+    ftp.cwd("/api/")
+    ftp.storlines("STOR out.menu.json", open("menu.json.out", 'rb'))
+
+def upload_image(f):
+    ftp = connect_to_ftp()
+    ftp.cwd("/api/divePhotos/")
+    ftp.storlines("STOR " + f.filename, f)
+
 @app.route('/')
-def student():
-   return render_template('student.html')
+def form():
+   return render_template('form.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form
+      f = request.files['image']
+
+      print(f)
+
+      download_json()
+      for x in result:
+          print(x, result[x])
+      edit_json(result, f.filename)
+
+      upload_json()
+      upload_image(f)
+
       return render_template("result.html",result = result)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host = "0.0.0.0", debug = True)
